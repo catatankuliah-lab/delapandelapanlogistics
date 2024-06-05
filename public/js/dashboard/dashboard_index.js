a039d73fd883feeaeff1de4249b3de4c615c93c9
catatankuliah-lab <nanasupriatnakursus@gmail.com>
2024-06-05 17:20:40 +0700

    setup project 4

---
+++ public/js/dashboard/dashboard_index.js
@@ -0,0 +1,181 @@
+function dashboard() {
+    alert("DASHBOARD");
+    $('#konten').append(
+        `<div class="container-xxl flex-grow-1 container-p-y">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="mb-3">
+                <div class="divider text-start">
+                  <div class="divider-text">
+                    <span class="menu-header-text fs-6">Dalam Data</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 mb-4">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between flex-sm-row flex-column gap-3">
+                    <div class="d-flex flex-sm-column flex-row align-items-start justify-content-between">
+                      <div class="card-title">
+                        <h5 class="text-nowrap mb-2">Konselor</h5>
+                      </div>
+                      <div class="mt-sm-auto">
+                        <span class="mb-0 fs-1">10</span>
+                      </div>
+                    </div>
+                    <div id="">
+                      <i class="tf-icons bx bx-user text-primary" style="font-size: 5rem !important;"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 mb-4">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between flex-sm-row flex-column gap-3">
+                    <div class="d-flex flex-sm-column flex-row align-items-start justify-content-between">
+                      <div class="card-title">
+                        <h5 class="text-nowrap mb-2">Konselor</h5>
+                      </div>
+                      <div class="mt-sm-auto">
+                        <span class="mb-0 fs-1">10</span>
+                      </div>
+                    </div>
+                    <div id="">
+                      <i class="tf-icons bx bx-user text-secondary" style="font-size: 5rem !important;"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 mb-4">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between flex-sm-row flex-column gap-3">
+                    <div class="d-flex flex-sm-column flex-row align-items-start justify-content-between">
+                      <div class="card-title">
+                        <h5 class="text-nowrap mb-2">Konselor</h5>
+                      </div>
+                      <div class="mt-sm-auto">
+                        <span class="mb-0 fs-1">10</span>
+                      </div>
+                    </div>
+                    <div id="">
+                      <i class="tf-icons bx bx-user text-info" style="font-size: 5rem !important;"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 mb-4">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between flex-sm-row flex-column gap-3">
+                    <div class="d-flex flex-sm-column flex-row align-items-start justify-content-between">
+                      <div class="card-title">
+                        <h5 class="text-nowrap mb-2">Konselor</h5>
+                      </div>
+                      <div class="mt-sm-auto">
+                        <span class="mb-0 fs-1">10</span>
+                      </div>
+                    </div>
+                    <div id="">
+                      <i class="tf-icons bx bx-user text-success" style="font-size: 5rem !important;"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-12 mb-4 order-0">
+              <div class="card">
+                <div class="d-flex align-items-end row">
+                  <div class="col-sm-9">
+                    <div class="card-body">
+                      <h5 class="card-title text-primary">Congratulations John! 🎉</h5>
+                      <p class="mb-4">
+                        You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
+                        your profile.
+                      </p>
+                    </div>
+                  </div>
+                  <div class="col-sm-3 text-center text-sm-left">
+                    <div class="card-body pb-0 px-0 px-md-4">
+                      <img src="../assets/img/illustrations/man-with-laptop-light.png" height="140" alt="View Badge User"       data-app-dark-img="illustrations/man-with-laptop-dark.png" data-app-light-img="illustrations/man-with-laptop-light.     png" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="mb-3">
+                <div class="divider text-start">
+                  <div class="divider-text">
+                    <span class="menu-header-text fs-6">Kegiatan</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+              <div class="card mb-3">
+                <img class="card-img-top" src="../assets/img/elements/18.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                  <p class="card-text mt-3">
+                    This is a wider card with supporting text below as a natural lead-in to additional content. This
+                    content is a little bit longer.
+                  </p>
+                  <p class="card-text">
+                    <a href="">
+                      <small class="text-primary">Selengkapnya</small>
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+              <div class="card mb-3">
+                <img class="card-img-top" src="../assets/img/elements/18.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                  <p class="card-text mt-3">
+                    This is a wider card with supporting text below as a natural lead-in to additional content. This
+                    content is a little bit longer.
+                  </p>
+                  <p class="card-text">
+                    <a href="">
+                      <small class="text-primary">Selengkapnya</small>
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+              <div class="card mb-3">
+                <img class="card-img-top" src="../assets/img/elements/18.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                  <p class="card-text mt-3">
+                    This is a wider card with supporting text below as a natural lead-in to additional content. This
+                    content is a little bit longer.
+                  </p>
+                  <p class="card-text">
+                    <a href="">
+                      <small class="text-primary">Selengkapnya</small>
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>`
+    );
+}
--- public/js/mainjs.js
+++ public/js/mainjs.js
@@ -1,1 +1,64 @@
-$('#dsh').text("DASHBOARD BARU");
+
+
+
+$(document).ready(function () {
+    // Fungsi Logout
+    function logout() {
+        Swal.fire({
+            title: 'Logout',
+            text: 'Apakah Anda yakin?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Ya, Logout!',
+            cancelButtonText: 'Batalkan!',
+            confirmButtonColor: '#696CFF',
+            cancelButtonColor: '#8492A2'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    icon: "success",
+                    title: "Logout",
+                    text: "Logout berhasil",
+                    showConfirmButton: false,
+                    timer: 3000,
+                }).then(() => {
+                    window.location.href = "auth/logout";
+                });
+            }
+        });
+    }
+
+    // Mengatur Navigasi
+    $('#navigasi li.active').removeClass('active');
+    $('#dashboard').addClass('active');
+    $('#namapage').text('Dashboard');
+    $('#buttondashboard').on('click', function () {
+        $('#navigasi li.active').removeClass('active');
+        $('#dashboard').addClass('active');
+        $('#namapage').text('Dashboard');
+        dashboard();
+    });
+    $('#buttonmobil').on('click', function () {
+        $('#navigasi li.active').removeClass('active');
+        $('#mobil').addClass('active');
+        $('#namapage').text('Mobil');
+    });
+    $('#buttondriver').on('click', function () {
+        $('#navigasi li.active').removeClass('active');
+        $('#driver').addClass('active');
+        $('#namapage').text('Driver');
+    });
+    $('#buttontambahmaintenance').on('click', function () {
+        $('#navigasi li.active').removeClass('active');
+        $('#tambahmaintenance').addClass('active');
+        $('#namapage').text('Tambah Maintenance');
+    });
+    $('#buttonriwayatmaintenance').on('click', function () {
+        $('#navigasi li.active').removeClass('active');
+        $('#riwayatmaintenance').addClass('active');
+        $('#namapage').text('Riwayat Maintenance');
+    });
+    $('#buttonlogout').on('click', function () {
+        logout();
+    });
+});
